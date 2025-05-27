7c4220490c557a56efeee8967fafac684b6d81de
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-05-27 20:05:45 +0000

    dunno

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lynn Lukose
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  if (localStorage.hits) {
+    displayCount()
+  }
+  else {
+    localStorage.setItem('hits', 0)
+    displayCount()
+  }
+}
+
+function clickCookie() {
+  localStorage.hits = Number(localStorage.hits) + 1
+  displayCount()
+}
+
+function displayCount() {
+  document.getElementById('result').innerHTML = "<h5>Total Hits : " + localStorage.hits + "</h5>"
+}
